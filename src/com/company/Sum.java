54e59e6e2f5f175290215418e47f58ba225b2a12
Harsh Karna <harshkarna4@gmail.com>
2021-12-12 18:35:33 +0530

    initial commit

---
+++ src/com/company/Sum.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Sum{
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=5;
+        int b=8;
+
+        System.out.println(a+b);
+    }
+}
