c20b693b3df2eada4c46a7a62ab1ae8d976acbb5
Harsh Karna <harshkarna4@gmail.com>
2021-12-12 18:50:45 +0530

    added Multiply.java

---
+++ src/com/company/multiply.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class multiply {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a=5;
+        int b=8;
+
+        System.out.println("The sum is "+ a*b);
+    }
+}
